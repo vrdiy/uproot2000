8db01357c18eba2e91251f2ef74d02a24ade0101
vrdiy <amverdigo@gmail.com>
2023-02-05 23:58:40 -0500

    physics module wip

--- physics.py
+++ physics.py
@@ -5,9 +5,18 @@ from paths import PROJECT_ROOT, RESOURCES_DIR
 from actor import Actor
 import collision
 from random import randrange
+import numpy as np
 
 
 class RigidBody():
     def __init__(self):
         self.velocity = (0,0,0)
-        
+
+
+def solveCollision(obj1,obj2,deltaTime):
+    v1 = obj1.velocity
+    v2 = obj2.velocity
+
+    obj1_acc = (v2 - v1) / deltaTime
+    obj2_acc = (v1 - v2) / deltaTime
+    
